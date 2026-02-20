1688651838b189b67af1d185a405ea5375fd4d92
Ethan Lafrenais <ethan.lafrenais@gmail.com>
2026-02-19 19:04:11 -0500

    hopefully fixed sound crashes

--- src/sound_rando.c
+++ src/sound_rando.c
@@ -1,5 +1,7 @@
 #include "modding.h"
+#include "dbgui.h"
 
+#include "PR/libaudio.h"
 #include "libnaudio/n_libaudio.h"
 #include "libnaudio/n_sndplayer.h"
 #include "libnaudio/n_sndp.h"
@@ -14,21 +16,17 @@
 #include "sys/main.h"
 #include "sys/rand.h"
 
-extern sndstate *g_SndpFreeStatesHead;
-extern void func_800668A4(void);
+#define SNDSTATEFLAG_01 0x01
+#define SNDSTATEFLAG_02 0x02
+#define SNDSTATEFLAG_04 0x04
+#define SNDSTATEFLAG_10 0x10
+#define SNDSTATEFLAG_20 0x20
 
-/** 
- * Fade out all currently playing sounds if we run out of state slots.
- * The SFX randomizer often starts bugged(?) sounds that never end which will eventually
- * stop all sounds from playing.
- */
-RECOMP_HOOK("func_80066064") void func_80066064_hook(void) {
-    // TODO: this causes crashes occasionally! figure out why
-    if (g_SndpFreeStatesHead == NULL) {
-        recomp_printf("too many sounds!!!\n");
-        func_800668A4();
-    }
-}
+extern sndstate *g_SndpAllocStatesHead;
+extern sndstate *g_SndpAllocStatesTail;
+extern sndstate *g_SndpFreeStatesHead;
+extern N_ALSndPlayer *g_SndPlayer;
+extern sndstate *func_80066064(ALBank *bank, ALSound *sound);
 
 #include "recomp/dlls/engine/6_AMSFX_recomp.h"
 
@@ -39,6 +37,58 @@ extern ALBankFile *_bss_0;
 
 static u16 soundID;
 static SoundDef* soundDef;
+
+static s32 forceSFX = FALSE;
+static s32 forceSFXID = 0;
+static s32 soundRandoDebugMenuOpen = FALSE;
+
+RECOMP_CALLBACK("*", recomp_on_dbgui) void sound_rando_dbgui(void) {
+    if (dbgui_begin_main_menu_bar()) {
+        if (dbgui_begin_menu("Chaos")) {
+            dbgui_menu_item("Sound Rando", &soundRandoDebugMenuOpen);
+            dbgui_end_menu();
+        }
+        dbgui_end_main_menu_bar();
+    }
+
+    if (!soundRandoDebugMenuOpen) return;
+
+    if (dbgui_begin("Sound Rando", &soundRandoDebugMenuOpen)) {
+        dbgui_checkbox("Force SFX ID", &forceSFX);
+
+        if (forceSFX) {
+            if (dbgui_input_int("SFX ID", &forceSFXID)) {
+                if (forceSFXID < 0) forceSFXID = 0;
+                s32 max = _bss_0->bankArray[0]->instArray[0]->soundCount - 1;
+                if (forceSFXID > max) forceSFXID = max;
+            }
+        }
+
+        dbgui_separator();
+
+        if (dbgui_tree_node("Sound States")) {
+	        sndstate *state;
+
+            dbgui_text(" == ALLOC LIST ==");
+	        state = g_SndpAllocStatesHead;
+	        for (s32 i = 0; state; i++, state = (sndstate *)state->node.next) {
+                ALMicroTime timeLeft = state->endtime - g_SndPlayer->curTime;
+                dbgui_textf("[%d] soundnum: %d | timeleft: %d | state: %d", 
+                    i, state->soundnum, timeLeft, state->state);
+            }
+            dbgui_separator();
+            dbgui_text(" == FREE LIST ==");
+            state = g_SndpFreeStatesHead;
+	        for (s32 i = 0; state; i++, state = (sndstate *)state->node.next) {
+                dbgui_textf("[%d] soundnum: %d | state: %d", 
+                    i, state->soundnum, state->state);
+            }
+
+            dbgui_end_child();
+        }
+    }
+    dbgui_end();
+}
 
 RECOMP_HOOK_DLL(dll_6_func_DE8) void dll_6_func_DE8_hook(u16 soundID_, SoundDef* soundDef_) {
     soundID = soundID_;
@@ -58,6 +108,9 @@ RECOMP_HOOK_RETURN_DLL(dll_6_func_DE8) void dll_6_func_DE8_hook_ret(void) {
         // Randomize SFX sound IDs
         if (rand_next(0, 99) < (f32)recomp_get_config_double("random_sfx_chance")) {
             soundDef->bankAndClipID = rand_next(0, _bss_0->bankArray[0]->instArray[0]->soundCount - 1);
+            if (forceSFX) {
+                soundDef->bankAndClipID = forceSFXID;
+            }
         }
 
         // Randomize SFX pitch
@@ -68,37 +121,94 @@ RECOMP_HOOK_RETURN_DLL(dll_6_func_DE8) void dll_6_func_DE8_hook_ret(void) {
     }
 }
 
-RECOMP_PATCH void _bnkfPatchWaveTable(ALWaveTable *w, s32 offset, s32 table) {
-	if (w->flags) {
-		return;
+RECOMP_PATCH sndstate *some_sound_func(ALBank *bank, s16 soundnum, u16 vol, ALPan pan, f32 pitch, u8 fxmix, u8 fxbus, sndstate **handleptr) {
+	sndstate *state;
+	sndstate *state2 = NULL;
+	ALKeyMap *keymap;
+	ALSound *sound;
+	s16 sp4e = 0;
+	s32 sp48;
+	s32 sp44;
+	s32 sp40 = 0;
+	s32 abspan;
+	N_ALEvent evt;
+	N_ALEvent evt2;
+
+	if (soundnum != 0) {
+		do {
+			sound = bank->instArray[0]->soundArray[soundnum - 1];
+			state = func_80066064(bank, sound);
+
+			if (state != NULL) {
+				g_SndPlayer->target = (s32)state;
+				evt.type = AL_SNDP_PLAY_EVT;
+				evt.msg.generic.sndstate = state;
+				abspan = pan + state->pan - AL_PAN_CENTER;
+
+				if (abspan > 127) {
+					abspan = 127;
+				} else if (abspan < 0) {
+					abspan = 0;
+				}
+
+				state->pan = abspan;
+				state->vol = (u32)(vol * state->vol) >> 15;
+				state->pitch *= pitch;
+				state->fxmix = fxmix;
+				state->fxbus = fxbus;
+                // @recomp: Use unused field for debugging the soundnum
+                state->soundnum = soundnum;
+
+				sp44 = sound->keyMap->velocityMax * 33333;
+
+				if (state->flags & SNDSTATEFLAG_10) {
+					state->flags &= ~SNDSTATEFLAG_10;
+					n_alEvtqPostEvent(&g_SndPlayer->evtq, &evt, sp40 + 1, 0);
+					sp48 = sp44 + 1;
+					sp4e = soundnum;
+				} else {
+					n_alEvtqPostEvent(&g_SndPlayer->evtq, &evt, sp44 + 1, 0);
+				}
+
+                // @recomp: Force SFX to not loop if SFX rando is enabled. For the SFX randomizer, looping sounds
+                //          will often play and never end which can be a bit overwhelming.
+                if (bank == _bss_0->bankArray[0] && recomp_get_config_double("random_sfx_chance") > 0) {
+                    state->flags &= ~SNDSTATEFLAG_02;
+                }
+
+				state2 = state;
+				if (1) {}
+			} else {
+                osSyncPrintf("Sound state allocate failed - sndId %d\n", soundnum);
+            }
+
+			sp40 += sp44;
+			keymap = sound->keyMap;
+			soundnum = keymap->velocityMin + (keymap->keyMin & 0xc0) * 4;
+		} while (soundnum && state);
+
+		if (state2 != NULL) {
+			state2->flags |= 0x1;
+			state2->unk30 = handleptr;
+
+			if (sp4e != 0) {
+				state2->flags |= SNDSTATEFLAG_10;
+
+				evt2.type = AL_SNDP_0200_EVT;
+				evt2.msg.generic.sndstate = state2;
+				evt2.msg.generic.data = sp4e;
+				evt2.msg.generic.data2 = bank;
+
+				n_alEvtqPostEvent(&g_SndPlayer->evtq, &evt2, sp48, 0);
+			}
+		}
 	}
 
-	w->flags = 1;
-
-	w->base += table;
-
-    // @recomp: Disable looping on sounds if enabled. For the SFX randomizer, looping sounds
-    //          will often play and never end otherwise which can be a bit overwhelming.
-    // TODO: this currently affects music/ambient banks! we don't want that!
-	if (w->type == AL_ADPCM_WAVE) {
-		w->waveInfo.adpcmWave.book  = (ALADPCMBook *)((u8 *)w->waveInfo.adpcmWave.book + offset);
-
-        if (recomp_get_config_u32("disable_sfx_looping")) {
-            w->waveInfo.adpcmWave.loop = 0;
-        } else {
-            if (w->waveInfo.adpcmWave.loop) {
-                w->waveInfo.adpcmWave.loop = (ALADPCMloop *)((u8 *)w->waveInfo.adpcmWave.loop + offset);
-            }
-        }
-	} else if (w->type == AL_RAW16_WAVE) {
-        if (recomp_get_config_u32("disable_sfx_looping")) {
-            w->waveInfo.rawWave.loop = 0;
-        } else {
-            if (w->waveInfo.rawWave.loop) {
-                w->waveInfo.rawWave.loop = (ALRawLoop *)((u8 *)w->waveInfo.rawWave.loop + offset);
-            }
-        }
+	if (handleptr != NULL) {
+		*handleptr = state2;
 	}
+
+	return state2;
 }
 
 #include "recomp/dlls/engine/5_AMSEQ_recomp.h"
@@ -164,6 +274,10 @@ RECOMP_PATCH s32 amseq_set(Object *obj, u16 actionNo, const char *filename, s32 
         // ambience
         if (rand_next(0, 99) < (f32)recomp_get_config_double("random_ambience_chance")) {
             sMusicAction->seqID = rand_next(0, sSeqFiles[0]->seqCount - 1);
+            // Note: Don't play ambient seq 3, it will crash
+            if (sMusicAction->seqID == 3) {
+                sMusicAction->seqID = 0;
+            }
         }
     } else {
         // music
@@ -222,6 +336,10 @@ RECOMP_PATCH void amseq_play_ex(u8 playerNo, u8 seqID, s16 bpm, s16 volume, u16 
         // ambience
         if (rand_next(0, 99) < (f32)recomp_get_config_double("random_ambience_chance")) {
             seqID = rand_next(0, sSeqFiles[0]->seqCount - 1);
+            // Note: Don't play ambient seq 3, it will crash
+            if (seqID == 3) {
+                seqID = 0;
+            }
         }
     } else {
         // music
